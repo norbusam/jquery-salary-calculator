ee1fa2ce56806fb9ea38cf56407a98e50bf5afd5
Norbu Samphel <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-02 17:11:19 -0500

    added submit button in html. added an event listener on the button

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@ console.log('JS loaded');
 
 $('document').ready(onReady);
 
+let employees = [];
+
 function onReady(){
     console.log('JQ loaded');
-}
+    $('#submitBtn').on('click', addEmployee);
+}// end of onReady
+
+function addEmployee(){
+    console.log('Submit logged');
+}// end of addEmployee
