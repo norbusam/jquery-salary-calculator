b4b61f66bc0d8187d0c0b42c48b52dbe67ddbf11
Norbu Samphel <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-04 19:59:01 -0500

    added sweet alert to delete function

--- scripts/script.js
+++ scripts/script.js
@@ -75,6 +75,24 @@ function appendToDom(){
 
 function deleteEmp(){
     console.log('deleted');
-    $(this).parent().parent().remove();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+        $(this).parent().parent().remove();
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+    // $(this).parent().parent().remove();
 }//end of deleteEm
 
